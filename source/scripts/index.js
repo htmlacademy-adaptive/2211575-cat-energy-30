22593ee7f7b671a8b7c277e92566e8554e5480b9
Artem Rizakhanov <107078736+cicimor@users.noreply.github.com>
2024-01-31 19:53:16 +0300

    вроде все ( устал

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,46 +1,15 @@
-const navMain = document.querySelector('.navigation');
-const navToggle = document.querySelector('.navigation__toogle');
+/* в этот файл добавляет скрипты*/
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-nav__toggle');
 
-navMain.classList.remove('navigation--nojs');
+navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('navigation--closed')) {
-    navMain.classList.remove('navigation--closed');
-    navMain.classList.add('navigation--opened');
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
   } else {
-    navMain.classList.add('navigation--closed');
-    navMain.classList.remove('navigation--opened');
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
   }
 });
-
-initMap();
-
-async function initMap() {
-  await ymaps3.ready;
-
-  const {YMap, YMapDefaultSchemeLayer, YMapDefaultFeaturesLayer, YMapMarker} = ymaps3;
-
-  const map = new YMap(
-    document.getElementById('map'),
-    {
-      location: {
-        center: [30.323037, 59.938631],
-        zoom: 16
-      }
-    }
-  );
-
-  const el = document.createElement('img');
-  el.className = 'my-marker';
-  el.src = '../images/icons/map-pin.svg';
-
-  map.addChild((scheme = new YMapDefaultSchemeLayer()));
-  map.addChild(new YMapDefaultFeaturesLayer());
-  map.addChild(new YMapMarker({coordinates: [30.323037, 59.938631]}, el));
-  const parent = el.parentElement;
-  if (window.matchMedia('(min-width: 768px)').matches) {
-    parent.style.width = '113px';
-  } else {
-    parent.style.width = '57px';
-  }
-}
